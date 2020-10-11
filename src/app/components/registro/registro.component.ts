22de333ccd14a316a273dfc465be365c8b0afb68
pasante12 <pasante12@gerardoortiz.com>
2020-10-11 01:50:25 -0500

    ListarAsignaturas

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EstudianteRestService } from '../../Codegen/api/estudianteRest.service';
 import { Estudiante } from '../../Codegen/model/estudiante';
 import { CarreraRestService } from '../../Codegen/api/carreraRest.service';
+import { Laboratorio } from '../../Codegen/model/laboratorio';
+import { LaboratorioRestService } from '../../Codegen/api/laboratorioRest.service';
 
 @Component({
   selector: 'app-registro',
@@ -10,41 +12,51 @@ import { CarreraRestService } from '../../Codegen/api/carreraRest.service';
 })
 export class RegistroComponent implements OnInit {
 
-  cedula:'';
-  estudiante:any;
-  nombre:'';
-  apellido:'';
-  id:0;
-  carrera:any;
-  nombrecarrera:'';
-  fecha=new Date();
-  
-  constructor(private estudianteservice:EstudianteRestService, private carreraservice: CarreraRestService) { }
+  cedula: '';
+  estudiante: any;
+  nombre: '';
+  apellido: '';
+  id: 2;
+  nivel: any;
+  niveles: '';
+  asignatura: '';
+  carrera: any;
+  nombrecarrera: '';
+  laboratorio: any;
+  fecha = new Date();
+  constructor(private estudianteservice: EstudianteRestService, private carreraservice: CarreraRestService, private laboratorioservice: LaboratorioRestService ) { }
 
   ngOnInit(): void {
    console.log(this.fecha);
+
   }
 
-  Buscar(){
+  Buscar() {
     console.log(this.cedula);
     this.estudianteservice.findByCedulaUsingGET(this.cedula).subscribe(data=>{
-      this.estudiante=data;
-      this.nombre=this.estudiante[0].nombre;
-      this.apellido=this.estudiante[0].apellido;
-      this.id=this.estudiante[0].id_carrera;
-      console.log(this.id);
-      console.log(this.estudiante);
+      this.estudiante = data;
+      this.nombre = this.estudiante[0].nombre;
+      this.apellido = this.estudiante[0].apellido;
+      this.id = this.estudiante[0].id_carrera;
+      
+
     });
-     
-    
+
   }
-  BuscarCarrera(){
-    this.carreraservice.findByIdUsingGET(this.id).subscribe(data=>{
-      this.carrera=data;
-      this.nombrecarrera=this.carrera[0].carrera_nombre
-      console.log(this.carrera)
-    })
+  BuscarCarrera() {
+    this.carreraservice.findByIdUsingGET(1).subscribe(data => {
+      this.carrera = data;
+      this.nombrecarrera = this.carrera[0].carrera_nombre;
+      this.nivel = this.carrera[0].niveles;
+      this.asignatura = this.carrera[0].niveles[0].asignaturas;    
+    });
+  }
+  BuscarLaboratorio() {
+    this.laboratorioservice.listLaboratorysUsingGET().subscribe(data => {
+      this.laboratorio = data;
+      console.log(this.laboratorio);
+    });
   }
   
+  }
 
-}
